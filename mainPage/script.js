8007e441a7c04c154525ee92156fd64634eabe92
llyqq134 <kirik0041@gmail.com>
2024-01-07 21:55:09 +0300

    отправление запроса + изменение активности кнопки

--- mainPage/script.js
+++ mainPage/script.js
@@ -1,1 +1,40 @@
+$('input[name="checkbox"]').on("change", function (e) {
+  if ($(this).is(":checked")) $(".button").attr("disabled", false);
+  else $(".button").attr("disabled", true);
+});
 
+
+$(document).ready(function() {
+  $('.formcarryForm').submit(function(event) {
+    event.preventDefault();
+    var href = $(this).attr("action");
+    const data = new FormData(this);
+
+    try {
+      for (value of data.values()) {
+        if (value == "") throw new Error("field validtaion error");
+      }
+    } catch (error) {
+      alert("Не все данные заполнены");
+      console.log(error);
+      return;
+    }
+
+    $.ajax({
+      type: "POST", 
+      url: href, 
+      data: data, 
+      contentType: false, 
+      processData: false, 
+      headers: {
+        'Accept': 'text/html'
+      },
+      success: function(response) {
+        alert('Форма успешно отправлена');
+      },
+      error: function(error) {
+        alert('Произошла ошибка при отправке формы\nно мы разберемся!');
+      }
+    });
+  });
+});
